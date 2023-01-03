b21a4c03b9c732ca59b478acdf91d6b37cdae301
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-01-03 16:17:20 +0530

    Add existing project files to Git

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const generator = require("generate-password");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let password;
+
+app.get("/", (req, res) => {
+    res.render("index", { password: password });
+});
+
+app.post("/", (req, res) => {
+    try {
+        password = generator.generate({
+            length: req.body.length,
+            numbers: req.body.numbers ? true : false,
+            uppercase: req.body.uppercase ? true : false,
+            lowercase: req.body.lowercase ? true : false,
+            symbols: req.body.symbols ? true : false,
+        });
+        res.redirect("/")
+    } catch (error) {
+        console.log("At least one setting must be selected");
+        res.redirect("/");
+    }
+});
+
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>404 - Oops! you seem to be lost</h1>");
+})
+
+app.listen(3000, () => {
+    console.log("Server is up and running on port 3000");
+})
